ff054e2eb0b53f26078cd3a30a61ade9c7e13c6f
Simon Gregory <simon@helvector.org>
2008-10-05 14:48:35 +0000

    • Adding project to class completions.
    git-svn-id: http://tribal.svn.beanstalkapp.com/snips/trunk/ActionScript%203%20Bundle%20Generator.tmbundle@19 42ab0dc8-6a81-42e3-8de5-31466c9b111c

--- Support/lib/as_source_to_completions.rb
+++ Support/lib/as_source_to_completions.rb
@@ -1,44 +1,100 @@
 #!/usr/bin/env ruby -wKU
 
+require 'fileutils'
+require ENV['TM_SUPPORT_PATH']+'/lib/textmate'
+
 # Class to convert all the files in the current TM project to files that can be
 # used by the the ActionScript 3 completion engine.
 #
 # This should essentially strip all comments and working code from the class
 # leaving the method signatures, public vars, and metadata etc in place.
-
-# Logic flow. 
-
+#
+# Logic flow.
 # 1. load class.
 # 2. load includes.
 # 3. strip comments.
 # 4. strip internal classes.
 # 5. strip any trailing imports.
-# 6. gather package, imports, metadata, class, and methods.
-
+# 6. gather package, imports, metadata, class, properties and methods.
+# 7. format and output gathered file data.
 class AsSourceToCompletions
 
 	private
 	
 	def initialize
 		@class = /^\s*(((dynamic|final)\s+)?(public)\s+((dynamic|final)\s+)?(class).*(?m:[^{]+))/
-		@imports = /^\s*import\s*[\w.*]+;?/
-		@methods = /^\s*(((override|public|protected|static)\s+)?(public|protected|static)\s+function\s+((get|set)\s+)?\b\w+\b\s*\(((?m:[^)]*))\)\s*:\s*(\w+))/		
+		@imports = /^\s*(import\s*[\w.*]+;?)/
+		@methods = /^\s*(((override|public|protected|static)\s+)?(public|protected|static)\s+function\s+((get|set)\s+)?\b\w+\b\s*\(((?m:[^)]*))\)(\s*:\s*(\w+))?)/		
 		@internals = /(^\s*package\s+[\w.]+(?m:[^$].*))((^(internal\s+)?class\b))/
 		@interface = /^\s*public\s+(interface)\s+(\w+)\b/
 		@metadata = /^(\[\s*\b(Effect|Event|Style)\b.*\])\s*$/
 		@package = /^\s*(package\s+[\w.]+)(?m:[^{]+)/
-    @vars = /^\s*\b(public|protected|static)\b\s+\b(public|protected|static)\b\s+\b(var|const)\b\s+\b(\w+)\b\s*:\s*((\w+)|\*)/
+    @vars = /^\s*(\b(public|protected|static)\b\s+(\b(public|protected|static)\b\s+)?\b(var|const)\b\s+\b(\w+)\b\s*:\s*((\w+)|\*))/
 	end	
 	
 	public
 	
+	def process_project
+		
+		bp = "/Users/simon/Desktop/mx"
+		pp = ENV['TM_PROJECT_DIRECTORY']
+		
+		file_count = 0
+		start_time = Time.new.to_f
+		
+		TextMate.each_text_file do |file|
+			
+			if file =~ /\.as$/
+				
+				file_count += 1
+				
+				path = file.sub( pp, "" );
+				fp = bp+path
+				
+				print "\nAdding: <a href=\"txmt://open/?url=file://#{file}\">#{path}</a> "
+
+				r = process(file)
+				
+				if r == "ERROR" or r.nil?
+					
+					print "<b>Failed</b><br/>\n"
+					
+				else
+					
+					FileUtils.mkdir_p File.dirname(fp)
+					
+					out = File.new( fp, File::WRONLY|File::TRUNC|File::CREAT )
+					out.print r
+					out.close
+					
+					print "<a href=\"txmt://open/?url=file://#{fp}\">OK</a><br/>\n"
+					
+				end
+
+			end
+			
+		end
+		
+		puts "<br/><h4>Complete</h4>"
+		puts "<br/>processed: #{file_count} files"
+		puts "<br/>in:  #{Time.new.to_f-start_time} seconds."	
+	end
+	
 	def process(uri)
 		
-		f = File.open(uri,"r" ).read.strip
+		fo = File.open(uri,"r" )
+		f = fo.read.strip
+		fo.close
 
 		doc = load_includes(f,uri)
-		doc	= parse(doc)
+		doc = strip_comments(doc)
+		#doc	= parse(doc)		
 		
+		doc.scan(@interface)		
+		return strip_whitespace(doc) if $1 == "interface"
+		
+		doc = strip_internals(doc)
+				
 		p = package(doc,true)
 		i = imports(doc,true)
 		d = metadata(doc,true)
@@ -46,9 +102,10 @@ class AsSourceToCompletions
 		m = methods(doc,true)
 		v = variables(doc,true)
 		
-		result = p + " {" + i + "\n" + d + "\n" + c.chomp + " {\n" + v + m + "\n}}"
-		result.gsub!("\n\n", "")
+		return "ERROR" if p.to_s == "" or c.to_s == ""
 		
+		result = p + " {\n" + i + "\n" + d + "\n" + c.chomp + " {\n" + v + m + "\n}}"		
+		result = strip_whitespace(result)		
 		result
 		
 	end
@@ -57,15 +114,10 @@ class AsSourceToCompletions
 
 		doc = strip_comments(doc)
 		
-		# Interfaces need not be processed
-		doc.scan(@interface)
+		doc.scan(@interface)		
 		return doc unless $1.nil?
 		
-		# Strips internal classes
-		doc.scan(@internals)
-		unless $1.nil?
-			doc = $1.sub( /\}((?m:[^}]+)\Z)/, "}\n")
-		end
+		doc = strip_internals(doc)
 				
 		doc
 		
@@ -79,6 +131,7 @@ class AsSourceToCompletions
 		end
 		
 		imps = doc.scan(@imports)
+		imps.uniq!
 		doc = imps.join("\n")
 		doc
 		
@@ -164,13 +217,10 @@ class AsSourceToCompletions
 		
 		m = []
 		doc.scan(@vars) do |l|
-			m << $&.to_s
-			#m << $1.gsub(/\s\s+|\n|\t/,'')
+			m << $1.to_s
 		end
 		
-		#m.each_index { |i| m[i] = m[i]+"{}" }
-		
-		doc = m.join("\n")
+		doc = m.join(";\n") + ";\n"
 		doc
 		
 	end
@@ -200,10 +250,21 @@ class AsSourceToCompletions
 
 		doc_a.each_index do |i|
 			line = doc_a[i]
-			if line =~ /include\s+"([\w.\/]+)"/
+			if line =~ /^\s*include\s+"([\w.\/]+)"/
+				
 				include_path = File.dirname(uri)+"/#{$1}"
-				include_file = File.open(include_path,"r").read.strip				
-				doc_a[i] = include_file.to_s
+				
+				if File.exists?(include_path)
+					
+					include_file = File.open(include_path,"r").read.strip				
+					doc_a[i] = include_file.to_s
+					
+				else
+					
+					puts "WARNING: #{include_path} 404."
+					
+				end
+				
 			end
 		end
 		
@@ -212,6 +273,23 @@ class AsSourceToCompletions
 		
 	end
 	
+	def strip_internals(doc)
+		
+			doc.scan(@internals)		
+			unless $1.nil?
+				doc = $1.sub( /\}((?m:[^}]+)\Z)/, "}\n")
+			end
+			
+			doc
+			
+	end
+	
+	def strip_whitespace(doc)
+		doc.gsub!( /^\s*$\n^\s*$\n/, '' )
+		doc.gsub!("\n\n", "\n")
+		doc
+	end
+
 end
 
 if __FILE__ == $0
@@ -447,6 +525,7 @@ package org.foo.bar
 			EOF
 			
 			result = <<-EOF
+public function Foo(){}
 public function set hello(value:String):void{}
 public function get hello():String{}
 public function sayFoo():String{}
@@ -459,6 +538,7 @@ EOF
 			assert_equal(result.chomp, p.methods(doc))
 
 		end
+
   end
 
 end
